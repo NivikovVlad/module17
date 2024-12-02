8084df9aea587eb14228785d4117dcf3e8aa8318
Ponedelnik <satewriter128@gmail.com>
2024-12-02 19:57:03 +0300

    m17_les2 готово

---
+++ app/models/user.py
@@ -0,0 +1,23 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+from app.backend.db import Base
+from sqlalchemy.schema import CreateTable
+# from app.models import *
+from app.models import task
+
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String)
+    firstname = Column(String)
+    lastname = Column(String)
+    age = Column(Integer)
+    slug = Column(String, unique=True, index=True)
+    tasks = relationship('Task', back_populates='user')
+
+
+if __name__ == "__main__":
+    print(CreateTable(User.__table__))
+    print(CreateTable(task.Task.__table__))
